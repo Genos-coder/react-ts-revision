e58c722b4c3137f568d22a6dbf44a43c28d70e3f
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-12-28 17:49:33 +0530

    feat traffic light chanllenge with state snapshot

---
+++ src/components/interactivity/challenge/TrafficLight.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+
+  function handleClick() {
+    //here if we use substitution method we know that after setWalk(!walk) we still have the walk  value true which is get by alert 
+      setWalk(!walk);
+      alert(walk?"Stop is next":"walk is next")
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1 style={{
+        color: walk ? 'darkgreen' : 'darkred'
+      }}>
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
+  );
+}
+
