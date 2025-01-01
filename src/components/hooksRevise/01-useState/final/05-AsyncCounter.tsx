7c662b0e524649c6dbd12ba04915941c48e2f7cd
Ganesh kale <ganeshdoitnowh100@gmail.com>
2025-01-01 16:31:41 +0530

    feat solving bug using updater function in from useState

---
+++ src/components/hooksRevise/01-useState/final/05-AsyncCounter.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+function AsyncCounter() {
+    const [count, setCount] = useState(0);
+
+    function handleClick(){
+        console.log("triggered")
+        // setTimeout(()=>{
+        //     setCount(count+1)
+        // },3000)
+        // above shit doesn't work we need to pass updater function to get the work done
+        setTimeout(()=>{
+            setCount(count=>{
+                return count+1
+            })
+        },3000)
+    }
+  return (
+    <div className="border border-black rounded-lg   p-4 flex justify-center items-center rounded-lgs flex-col gap-4"> 
+        <h2>{count}</h2>
+        <button className="bg-black rounded-lg text-white p-1" onClick={handleClick}>Increment</button>
+    </div>
+  )
+}
+export default AsyncCounter
